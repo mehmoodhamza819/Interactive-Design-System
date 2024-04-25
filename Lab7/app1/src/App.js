2d3732b3391583d048ba7d96450e02baf444faa9
mehmoodhamza819 <48319480+mehmoodhamza819@users.noreply.github.com>
2024-04-25 18:52:58 +0300

    Lab7

---
+++ Lab7/app1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import {useSatate,useEffect} from 'react';
+import APIKEY from './config';
+import './App.css';
+
+function App() {
+  return (
+    <div>This is my Application
+    </div>
+  );
+}
+
+export default App;
