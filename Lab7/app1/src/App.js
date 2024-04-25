36252426a36fbf45669e15eb8e03bbdec48f3a4e
mehmoodhamza819 <48319480+mehmoodhamza819@users.noreply.github.com>
2024-04-25 19:51:18 +0300

    Lab7 v2

--- Lab7/app1/src/App.js
+++ Lab7/app1/src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
-import {useSatate,useEffect} from 'react';
-import APIKEY from './config';
-import './App.css';
-
-function App() {
-  return (
-    <div>This is my Application
-    </div>
-  );
-}
-
+import {useState, useEffect} from 'react'; 
+import './App.css'; 
+import APIKey from './config'; 
+import Movie from './components/Movie'; 
+ 
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key='; 
+ 
+function App() { 
+  const [movieResults, setMovieResults] = useState([]); 
+  //const testArray = ['age',2,true,"aa"]; 
+ 
+  useEffect( ()=> { 
+      fetch(APIURL + APIKey) 
+      .then( (resp) => resp.json() ) 
+      .then( (data) => {
+      console.log(data);
+      setMovieResults(data.results);
+    }
+      ) 
+  } 
+  ,[]) 
+ 
+  return ( 
+ 
+    <> 
+    {movieResults.map( (element,index) => (<Movie key={index}/>) 
+     
+    )} 
+    </> 
+  ); 
+} 
+ 
 export default App;
