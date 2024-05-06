cb4c41c08f6d6e8803fa2955c032f734455efbad
mehmoodhamza819 <48319480+mehmoodhamza819@users.noreply.github.com>
2024-05-06 19:04:29 +0300

    lab7-poster-image

--- Lab7/app1/src/components/Movie.js
+++ Lab7/app1/src/components/Movie.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Movie.css";
+
 const ImageURL = 'https://image.tmdb.org/t/p/w500/';
+const placeholderImage = 'https://raw.githubusercontent.com/mehmoodhamza819/Interactive-Design-System/main/Lab7/app1/public/logo192.png';
 
 function Movie({poster_path,title,overview,vote_average})
 {
+    const ImageURLFinal=ImageURL+poster_path;
+    const imageURLFinal = poster_path ? ImageURL + poster_path : placeholderImage;
     return(
 <div className="movie">
-    <img src={ImageURL+poster_path}alt={title}></img>
+    <img src={imageURLFinal}alt={title}></img>
     <div className="movie-info">
         <h3>{title}</h3>
         <span>{vote_average}</span>
